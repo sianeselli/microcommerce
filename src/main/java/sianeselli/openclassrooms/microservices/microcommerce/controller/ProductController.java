bc617cf283a49bc1b0299ba9907b9bb980e38b62
sianeselli <sylvain.ianeselli@gmail.com>
2018-10-08 22:17:07 +0200

    Repository

--- src/main/java/sianeselli/openclassrooms/microservices/microcommerce/controller/ProductController.java
+++ src/main/java/sianeselli/openclassrooms/microservices/microcommerce/controller/ProductController.java
@@ -1,5 +1,7 @@
 package sianeselli.openclassrooms.microservices.microcommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,12 @@ public class ProductController {
 	
 	@Autowired
 	private ProductDao productDao;
+	
+	//Produits
+	@GetMapping(value="Produits")
+	public List<Product> listeProduits(){
+		return productDao.findAll();
+	}
 	
 	//Produits/{id}
 	@GetMapping(value="Produits/{id}")
