bdea21cfe184601c70cdf78647c41f467d9087f8
sianeselli <sylvain.ianeselli@gmail.com>
2018-10-08 21:42:50 +0200

    first commit

---
+++ src/main/java/sianeselli/openclassrooms/microservices/microcommerce/MicrocommerceApplication.java
@@ -0,0 +1,12 @@
+package sianeselli.openclassrooms.microservices.microcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocommerceApplication.class, args);
+	}
+}
